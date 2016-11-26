26052fedabe0cf29d19256a39bf8336faffc168c
mustang85 <sergeant-kir@mail.ru>
2016-11-26 15:26:55 +0300

    Validation username, email uniquenses on server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,29 +1,72 @@
 import express from 'express';
-import validateInput from '../shared/validations/signup';
+import commonValidations from '../shared/validations/signup';
 // import bcrypt from 'bcrypt';
+// import Promise from 'bluebird';
+import isEmpty from 'lodash/isEmpty';
 
 import User from '../models/users';
 
 let router = express.Router();
 
+function validateInput(data, otherValidations) {
+	let { errors } = otherValidations(data);
+
+	return User.query({
+		where: { email: data.email },
+		orWhere: { username: data.username }
+	}).fetch().then(user => {
+		if (user) {
+			if (user.get('username') === data.username) {
+				errors.username = 'There is user with such username';
+			}
+			if (user.get('email') === data.email) {
+				errors.email = 'There is user with such email';
+			}
+		}
+		return {
+			errors,
+			isValid: isEmpty(errors)
+		};
+	})
+
+	// return Promise.all([
+	// 	User.where({ email: data.email }).fetch().then(user => {
+	// 		if (user) {
+	// 			errors.email = 'There is user with such email';
+	// 		}
+	// 	}),
+
+	// 	User.where({ username: data.username }).fetch().then(user => {
+	// 		if (user) {
+	// 			errors.username = 'There is user with such username';
+	// 		}
+	// 	})	
+	// ]).then(() => {
+	// 	return {
+	// 		errors,
+	// 		isValid: isEmpty(errors)
+	// 	};
+	// });
+}
 
 router.post('/', (req, res) => {
 	setTimeout(() => {
-		const { errors, isValid } = validateInput(req.body);
-		console.log('req.body', req.body)
-		if (isValid) {
-			const { username, password, timezone, email } = req.body;
-			const password_digest = parseInt(password, 10);
+		validateInput(req.body, commonValidations).then(({ errors, isValid }) => {
+			console.log('req.body', req.body)
+			if (isValid) {
+				const { username, password, timezone, email } = req.body;
+				const password_digest = parseInt(password, 10);
 
-			User.forge({
-				username, timezone, email, password_digest
-			}, { hasTimestamps: true }).save()
-				.then(user => res.json({ success: true }))
-				.catch(err => res.status(500).json({ error: err }));
+				User.forge({
+					username, timezone, email, password_digest
+				}, { hasTimestamps: true }).save()
+					.then(user => res.json({ success: true }))
+					.catch(err => res.status(500).json({ error: err }));
 
-		} else {
-			res.status(400).json(errors);
-		}
+			} else {
+				res.status(400).json(errors);
+			}			
+		});
 
 		// if (!isValid) {
 		// 	res.status(400).json(errors);
